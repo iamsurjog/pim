9d3074914ef657e7e816c9318b2f901c55d9feb0
Sujatro Ganguli <iamsurjog@gmail.com>
2026-07-10 23:09:10 +0530

    init

---
+++ cmd/cache.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// cacheCmd represents the cache command
+var cacheCmd = &cobra.Command{
+	Use:   "cache",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("cache called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cacheCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cacheCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cacheCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 Sujatro Ganguli iamsurjog@gmail.com
+
+*/
+package main
+
+import "pim/cmd"
+
+func main() {
+	cmd.Execute()
+}
