e6eb70860cc2d745e488bdcc4b227f7cfe5123d9
Sujatro Ganguli <iamsurjog@gmail.com>
2026-07-13 11:08:52 +0530

    chore: gitignore + others

---
+++ NOTE.md
@@ -0,0 +1,2 @@
+> [!NOTE]
+> direnv install and setup required for lsps
--- cmd/init.go
+++ cmd/init.go
@@ -14,11 +14,13 @@ import (
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a fresh workspace manifest and select a Python version.",
-	Long: `Interactively creates a 'your_tool.toml' project configuration file in the current
+	Long: `Interactively creates a 'pim.json' project configuration file in the current
 working directory. It automatically scans your locally cached Python installations,
 prompts you to select the active interpreter version via an interactive menu, and
 hooks the workspace by writing an optimized local .env file for IDE autocompletion.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Enter python version to use:")
+		fmt.Scan()
 		fmt.Printf("not implemented %q yet\n", cmd.Short)
 	},
 }
